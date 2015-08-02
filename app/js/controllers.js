5885eb288f0320f93ca6cd56da5a5d9afe387eb6
ronatgithub <ronny.mombasa@gmail.com>
2015-08-02 13:10:04 +0300

    minor changes

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,8 +3,8 @@
 /* Controllers */
 
 angular.module('app.controllers', [])
-  .controller('MyCtrl1', [function() {
-
+  .controller('MyCtrl1', [function($scope) {
+  	// var db = new PouchDB('testdb_v3');
   }])
   .controller('MyCtrl2', [function() {
 
