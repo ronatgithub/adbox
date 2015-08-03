700d7e8559ab9c945819697b76c505d32fd2705d
ronatgithub <ronny.mombasa@gmail.com>
2015-08-03 12:53:37 +0300

    seting up pouchdb as factory with working controller. very basic implementation

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,7 +4,7 @@
 
 angular.module('app.controllers', [])
   .controller('MyCtrl1', [function($scope) {
-  	// var db = new PouchDB('testdb_v3');
+  	
   	var localDB = new PouchDB('adbox_v1');
   	var remoteDB = new PouchDB('https://adscreen.iriscouch.com/adbox_v1');
   	localDB.sync(remoteDB, {
@@ -24,6 +24,37 @@ angular.module('app.controllers', [])
 	  console.log('error, this shouldn`t happen')
 	});
   }])
-  .controller('MyCtrl2', [function() {
 
-  }]);
+// debugger;
+
+// taken from: http://jsfiddle.net/yoorek/2zt27/1/
+.controller('MyCtrl2', function ($scope, Database) {
+    var db = new Database('test_v1');
+
+    $scope.init = function () {
+        db.all()
+            .then(function (result) { 
+                $scope.data = result; 
+        });
+    };
+
+    $scope.populate = function () {
+        db.create({
+            name: 'One'
+        });
+        db.create({
+            name: 'Two'
+        });
+        db.create({
+            name: 'Three'
+        });
+    };
+
+    $scope.remove = function (item, index) {
+        db.remove(item);
+        $scope.data.splice(index, 1);
+    };
+
+    $scope.db = db;
+})
+
--- app/js/services.js
+++ app/js/services.js
@@ -5,30 +5,46 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('app.services', []).
-  value('version', '0.1');
+angular.module('app.services', [])
+.value('version', '0.1')
 
-// Inject the pouchdb object into your services
-// See: https://github.com/wspringer/angular-pouchdb#usage
-// angular.factory('someservice', function(pouchdb) {
-//   // Do something with pouchdb.
-// });
+// have no idea what it means, but works. taken from: http://jsfiddle.net/yoorek/2zt27/1/
+.factory('Database', function ($q) {
+    var _db,
+    _databaseName;
 
-// Injecting a database as a dependency
-// https://github.com/wspringer/angular-pouchdb#injecting-a-database-as-a-dependency
-// angular.module('app.services').
-//   factory('$db', function(pouchdb) {
-//     return pouchdb.create('testdb');
-// });
+    var Database = function (databaseName) {
+        _databaseName = databaseName;
+        _db = new PouchDB(_databaseName);
+    };
 
-// angular.module('app.services').
-//   factory('testservice', function($db) {
-//     return {
-//       add: function(obj) { $db.put(obj); }
-//     };
-// });
+    Database.prototype.all = function () {
+        var options = {
+            include_docs: true
+        };
 
-//  angular.module('app.services').service('syncService',
-//     function ($q, $log, $rootScope, storageService, pouchdb, config) {
-//     }
-// );
+        return $q.when(_db.allDocs(options))
+            .then(function (result) {
+            var converted;
+
+            converted = result.rows.map(function (element) {
+                return element.doc;
+            });
+
+            return converted;
+        });
+    };
+
+    Database.prototype.create = function (record) {
+        return $q.when(_db.post(record))
+            .then(function (result) {
+            return _db.get(result.id);
+        });
+    };
+
+    Database.prototype.remove = function (record) {
+        return $q.when(_db.remove(record));
+    };
+
+    return Database;
+});
